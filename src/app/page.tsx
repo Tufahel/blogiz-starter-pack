6f7e3a6ba9209add13ccf44aaaa6ecbae6d8ef4e
Tufahel Ahmed <tufahel96@gmail.com>
2024-07-09 20:45:26 +0600

    caching and revalidating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import LatestBlogs from "@/components/LagestBlogs/LatestBlogs";
+
 const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: {
+      revalidate: 10,
+    },
+  });
+  const blogs = await res.json();
+  // console.log(blogs);
   return (
     <>
-      <h1 className="text-center text-4xl my-5">Latest Blogs</h1>
+      <LatestBlogs blogs={blogs} />
     </>
   );
 };
