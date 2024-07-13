1ffff607a0bc0ba6497e72602e7d62ad7f80542f
Tufahel Ahmed <tufahel96@gmail.com>
2024-07-13 21:22:13 +0600

    Fetch dynamic data using generatestaticparams for ssg

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,21 +1,30 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
 
 interface BlogId {
-    params: {
-        blogId: string;
-    };
+  params: {
+    blogId: string;
+  };
 }
 
-const BlogDetailsPage = async ({params}: BlogId) => {
-    const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
-        cache: "no-store",
-    })
-    const blog = await res.json();
-    return (
-        <div>
-            <BlogDetails blog={blog}/>
-        </div>
-    );
+export const generateStaticParams = async () => {
+  const res = await fetch(`http://localhost:5000/blogs/`);
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
+const BlogDetailsPage = async ({ params }: BlogId) => {
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
+  const blog = await res.json();
+  return (
+    <div>
+      <BlogDetails blog={blog} />
+    </div>
+  );
 };
 
 export default BlogDetailsPage;
