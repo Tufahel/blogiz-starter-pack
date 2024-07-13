9abab1b22b939918544ec737be1657bb2d181e0d
Tufahel Ahmed <tufahel96@gmail.com>
2024-07-13 21:51:24 +0600

    Display blog data using redux and fix server address

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,16 @@
+"use client"
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/types";
 
-const BlogPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store"
-  });
-  const blogs = await res.json();
+const BlogPage = () => {
+  // const res = await fetch("http://localhost:5000/blogs", {
+  //   cache: "no-store"
+  // });
+  // const blogs = await res.json();
+
+  const {data:blogs, isLoading, isError, error} = useGetBlogsQuery("");
+  console.log(blogs);
   return (
     <div className="w-[90%] mx-auto">
       <h1 className="text-4xl text-center my-5">
@@ -16,7 +21,7 @@ const BlogPage = async () => {
         unprecedented computational power.
       </p>
       <div className="grid grid-cols-3 gap-4 my-5">
-        {blogs.map((blog: Blog) => (
+        {blogs?.map((blog: Blog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
       </div>
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,17 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import type { Pokemon } from './types'
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
   endpoints: (builder) => ({
     getBlogs: builder.query({
-      query: () => `/blogs`,
+      query: () => "/blogs",
     }),
   }),
 })
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetBlogsQuery } = baseApi
+export const { useGetBlogsQuery } = baseApi;
